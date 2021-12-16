ac5aec905f9c329fca32a8cdcba30b9ebf10d511
Okoye Peace Onyinye <nellypamicable@gmail.com>
2021-12-16 10:23:11 +0100

    second commit

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const userEmail=document.querySelector('#email').value;
 const activationToken=document.querySelector('#password').value;
 const checkbox=document.querySelector('#checkbox:checked');
 const form=document.querySelector('#form');
-console.log(checkbox);
 
 if (userName == ""){
     document.querySelector('#error').innerHTML="Name cannot be empty!!!";
@@ -17,18 +16,15 @@ else if (activationToken == ""){
     document.querySelector('#error').innerHTML="Please enter activation token!!!";
 }
 else if (activationToken.length !== 4){
-    document.querySelector('#error').innerHTML="Invalid token!!!";
+    document.querySelector('#error').innerHTML="Token must be four digits !!!";
 }
 else if (checkbox == null){
 
-    document.querySelector('#error').innerHTML="Please agree to our policy";
+    document.querySelector('#error').innerHTML="Please agree to our  policy";
 }
 else {
     document.querySelector('#error').innerHTML="Successful";
 }
-console.log('Name:',userName);
-console.log('Email:',userEmail);
-console.log('Token:',activationToken);
 }
 form.addEventListener('submit', submit);
 
